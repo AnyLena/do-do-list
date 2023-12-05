48ee79cddd0151aa35875c6d3d2153d4c59085ee
LenaBultmann <lena.bultmann@gmx.de>
2023-12-05 10:14:25 +0100

    resolving merge conflict

--- script.js
+++ script.js
@@ -2,26 +2,33 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 const deleteBtn = document.querySelectorAll(".deleteButton");
 
-
 function addTask() {
- if (inputBox.value === ''){
-  alert("You must write something!")
- }
- else {
-  let li= document.createElement("li");
-  li.innerHTML = inputBox.value;
-  listContainer.appendChild(li);
-  let btn = document.createElement("button");
-  btn.innerHTML = "X";
-  btn.classList.add("deleteButton")
-  li.appendChild(btn);
-  btn.addEventListener("click", deleteTask);
+   if (inputBox.value === ''){
+    alert("You must write something!")
+   }
+   else {
+    let li= document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let btn = document.createElement("button");
+    btn.innerHTML = "X";
+    btn.classList.add("deleteButton")
+    li.appendChild(btn);
+    btn.addEventListener("click", deleteTask);
+  }
+  inputBox.value = "";
 }
-}
+
+listContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+  }
+    else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+    }
+  }, false);
 
 
 const deleteTask = (e) => {
-e.target.parentElement.remove();
+  e.target.parentElement.remove();
 }
-
-deleteBtn.forEach( (e) => e.addEventListener("click", deleteTask));
